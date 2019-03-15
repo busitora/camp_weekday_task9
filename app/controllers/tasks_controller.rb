019e714f4c8d7dbe040af4a2096197ddf17b71a0
busitora <ts12272530@gmail.com>
2019-03-15 18:12:43 +0900

    細かい修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   
   def index
     # @tasks = Task.all
-    @tasks = Task.order(created_at: :desc).limit(5)
+    @tasks = Task.where(is_display: true).limit(5)
   end
 
   def show
